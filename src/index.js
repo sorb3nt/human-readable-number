8493185aca0945bd0f6ee1bccfcb4a437df828ef
sorb3nt <sorb3nt@gmail.com>
2022-07-05 16:22:25 +0300

    feat: add solution to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
 module.exports = function toReadable (number) {
-  
+    let rest;
+    let result = "";
+
+    let arr1 = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    let arr10 = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number === 0) return "zero";
+
+    rest = (number % 100).toString();
+
+    if (rest.length === 1) {
+        result = result + arr1[rest];
+    } else if (rest.length === 2 && rest < 20) {
+        result = result + arr1[rest];
+    } else if (rest.length === 2 && rest >= 20) {
+        result = result + arr10[rest[0]] + " " + arr1[rest[1]];
+    }
+
+    rest = (number % 1000).toString();
+
+    if (rest.length === 3) {
+        result = `${arr1[rest[0]]} hundred ${result}`;
+    }
+
+    return result.trim();
 }
